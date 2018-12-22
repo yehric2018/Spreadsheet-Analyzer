0ef4f63997aa9992635f5431f095e8985d3578ff
Eric <eric.yehj@outlook.com>
2018-12-22 14:37:16 -0800

    Added constructor to spreadsheet

--- TableFolder/src/SpreadSheet.java
+++ TableFolder/src/SpreadSheet.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 /**
  * A spreadsheet that implements the methods in Table and ultimately compares to another Spreadsheet
  */
 public class SpreadSheet implements Table {
-
+	private String[] header;
+	private String[][] data;
+	
+	public SpreadSheet(Scanner in) {
+		if (!in.hasNextLine()) {
+			throw new IllegalArgumentException();
+		}
+		
+		header = in.nextLine().split(",");
+		List<String[]> dataList = new ArrayList<String[]>();
+		while (in.hasNextLine) {
+			dataList.add(in.nextLine().split(","));
+		}
+		
+		data = new String[dataList.size()][header.length];
+		for (int i = 0 ; i < dataList.size(); i++) {
+			data[i] = dataList.get(i);
+		}
+	}
+	
 	@Override
 	public Table compare(Table other) {
 		// TODO Auto-generated method stub
